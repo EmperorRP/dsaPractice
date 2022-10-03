0c56088fb436ea179360e7d9f7076a5e73652ff7
emperorrp <rahulpujari2919@gmail.com>
2022-10-03 20:50:16 +0530

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(5);
+        list.insertFirst(8);
+        list.insertFirst(9);
+        list.insertFirst(2);
+        list.insertLast(82);
+//        list.insert(20, 2);
+//        list.deleteFirst();
+//        list.deleteLast();
+//        list.Display();
+//        list.delete(3);
+//        list.Display();
+        list.Display();
+//        list.insertRec(88, 2);
+        list.Display();
+
+
+
+
+//        DLL list = new DLL();
+//        list.insertFirst(5);
+//        list.insertFirst(8);
+//        list.insertFirst(9);
+//        list.insertFirst(2);
+//        list.insertLast(89);
+//        list.insert(8, 42);
+//        list.display();
+
+
+    }
+}
